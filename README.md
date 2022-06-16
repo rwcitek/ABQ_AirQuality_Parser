63c2789eb8865a7c13c5b4f5f8c653189009c9cc
Ed Howland <ed.howland@gmail.com>
2022-06-16 17:43:02 -0400

    added abqaq.py and updated README.md with install/setup/run instructions

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # AbqAq_Parser
 Parser for Abq, N.M. Air Quality data format
+
+
+## Abstract
+
+This file: abqaq.py will parse a file downloaded from Abq N.M.'s web site for air quality readings
+
+It currently just checks the syntax of the file.
+If the data file parses correctly, it will output a badly formed AST (list of
+lists) and exit with a 0 status code.
+
+
+If there is some fault in the data file or the program itself, it prints an error message
+on stderr and exits with a status code of 1.
+
+## Setup
+
+Note: If running on a Debian-derived system, replace the python and pips with python3 and pip3.
+
+This program works best with Python version 3.10. It may work with older versions, but they have not been tested.
+
+Note: This step is optional.
+
+0. Create a virtual environment.
+
+```bash
+python -m venv .venv
+```
+
+1.  Install the dependancies
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Dowload a data file
+
+```bash
+curl  http://data.cabq.gov/airquality/aqindex/history/042222.0017  > data.dat
+```
+
+
+
+3. Run the program
+
+```bash
+python abqaq.py < data.dat
+```
---
+++ abqaq.py
@@ -0,0 +1,83 @@
+""" abaq.py : Attempt to parse data from Albuquerque, NM Air quality sensor data """
+import sys
+from  parsy import seq,string,regex
+
+
+# helper functions
+def xclude_empties(*args):
+  return [x for x in args if x is not None]
+
+# Terminals
+
+cr = string("\r")
+lf = string("\n")
+crlf = cr  +  lf
+el=crlf # Use this alias for the type of line endings. Orig file has Cr Lf el s
+caps = regex(r'[A-Z]')
+
+## Terminal literals
+
+bd = string("BEGIN_DATA")
+ed = string("END_DATA")
+bg=string('BEGIN_GROUP')
+eg=string('END_GROUP')
+bf=string('BEGIN_FILE')
+ef=string('END_FILE')
+
+
+
+# TODO: Rename k1 to Key
+k1=regex(r'[A-Za-z0-9._\-\/ ]+')
+id=regex(r'[0-9]+')
+comma = string(",")
+num=regex(r'-?[0-9]+(\.[0-9]+)?')
+
+#  combined parsers
+
+kv = seq(k1 << comma, id)
+# the difference between '<<' and '>>' opers is: which side of the  oper is preserved in seq()
+# If you want to exclude the leading ',' then >>. Or if it is a trailing then <<
+#  IOW: the angles point to the thing you want to preserve
+# The .many() turns into a regex-like '*' operation
+vl = (comma >> num).many()
+vc = (comma >> caps).many()
+
+# Debug stuff here
+def rddata(fn):
+  """ rddata() returns the _ exact _ contenst of 'begin_data.ex1' """
+  with open(fn, newline='') as f:  # Suppress the line ending handler
+    return f.read()
+
+
+
+
+#dbg = (bd >> lf >> seq(k1 << comma, num) + vlist << lf >> seq(k1 << comma, num) + vlist << lf << ed)
+v=(comma >> num).many()
+dbg = seq(bd, lf, k1, comma, num, v, lf, k1, comma, num, v, lf, ed)
+
+# NonTerminals
+Value = k1
+
+# This captures the TZONE,MST,7 in the original file
+KeyValueOpt = seq(k1, comma >> Value, (comma >> Value).optional()).combine(xclude_empties)
+
+
+DataLine = seq(el >> kv, vl, el >> kv, vc).at_least(1)
+DataSection = seq(bd, DataLine, el >> ed << el)
+
+GroupSection = seq(bg, (el >> KeyValueOpt).at_least(1), el >> DataSection, eg << el)
+
+GroupSectionList = GroupSection.at_least(1)
+
+FileSection = seq(bf, (el >> KeyValueOpt).at_least(1), el >> GroupSectionList, ef << el)
+
+def main():
+  x=sys.stdin.read()
+  try:
+    print(FileSection.parse(x))
+  except Exception as exc:
+    print(exc, file=sys.stderr)
+    exit(1)
+
+if __name__ == "__main__":
+  main()
